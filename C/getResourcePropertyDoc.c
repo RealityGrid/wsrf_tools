fdd4e303351fa7c22f2ce0cafa08bf5f9656552e
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-10-13 08:51:47 +0000

    [C] Added new tool for obtaining the resource property document from an SWS

---
+++ C/getResourcePropertyDoc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "soapH.h"
+#include "ReG_Steer_types.h"
+#include "ReG_Steer_Steerside_WSRF.h"
+
+/*------------------------------------------------------------*/
+
+int main(int argc, char **argv){
+
+  struct soap mySoap;
+  char       *rpDoc;
+
+  if(argc != 2){
+    printf("Usage:\n  getResourcePropertyDoc <EPR of SWS>");
+    return 1;
+  }
+
+  soap_init(&mySoap);
+
+  Get_resource_property_doc(&mySoap,
+			    argv[1],
+			    &rpDoc);
+	 
+  fprintf(stdout, "Resource property document:\n>>%s<<\n", rpDoc);
+
+  soap_end(&mySoap);
+  soap_done(&mySoap);
+  return 0;
+}
