b3a5257154dbe89efa8ae1610ca219856c09e96d
Andrew Porter <andrew.porter@manchester.ac.uk>
2005-11-29 17:26:48 +0000

    [perl] Added both SSL and WSSE options - work in progress

--- perl/Destroy.pl
+++ perl/Destroy.pl
@@ -4,13 +4,15 @@ BEGIN {
        @INC = ( @INC, "/home/zzcguap/projects/WSRF-Lite" );
 };
 
-#use WSRF::Lite +trace =>  debug => sub {};
-use WSRF::Lite;
+use WSRF::Lite +trace =>  debug => sub {};
+#use WSRF::Lite;
+use MIME::Base64;
+use Digest::SHA1 qw(sha1 sha1_hex sha1_base64);;
 use strict;
 
 #need to point to users certificates - these are only used
 #if https protocal is being used.
-$ENV{HTTPS_CA_DIR} = "/etc/grid-security/certificates/";
+#$ENV{HTTPS_CA_DIR} = "/etc/grid-security/certificates/";
 $ENV{HTTPS_CERT_FILE} = $ENV{HOME}."/.globus/usercert.pem";
 $ENV{HTTPS_KEY_FILE}  = $ENV{HOME}."/.globus/userkey.pem";
 
@@ -23,6 +25,36 @@ if( @ARGV < 1 )
   exit;
 }
 
+# seed the random number generator
+srand (time ^ $$ ^ unpack "%L*", `ps axww | gzip`);
+my $num = rand 1000000;
+my $nonce = encode_base64("$num", "");
+
+my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime(time);
+# month returned by localtime is zero-indexed and we need to convert to
+# a four-digit date...
+my $created = sprintf "%4d-%02d-%02dT%02d:%02d:%02dZ",
+                     $year+1900,$mon+1,$mday,$hour,$min,$sec;
+
+print "Enter SWS passphrase: ";
+my $passphrase = <STDIN>;
+chomp($passphrase);
+print "\n";
+#my $passphrase = "somethingcunning";
+
+# Password digest = Base64( SHA-1(nonce + created + password) )
+my $digest = sha1_base64($nonce . $created . $passphrase);
+
+my $user = SOAP::Data->name('wsse:Username' => 'Andy_Porter');
+my $passwd = SOAP::Data->name('wsse:Password' => $digest);
+my $nonce = SOAP::Data->name('wsse:Nonce' => $nonce);
+my $created = SOAP::Data->name('wsse:Created' => $created);
+
+my $hdr1 = SOAP::Data->new(name => 'wsse:UsernameToken', 
+			   value => \SOAP::Data->value($user,
+						       $passwd,
+						       $nonce,
+						       $created));
 my $target;
 #get the location of the service
 while(defined($target = shift @ARGV)){
@@ -30,16 +62,18 @@ while(defined($target = shift @ARGV)){
     my $ans=  WSRF::Lite
 	-> uri($WSRF::Constants::WSRL)
 	-> wsaddress(WSRF::WS_Address->new()->Address($target))  
-	-> Destroy();
+	-> Destroy(SOAP::Header->name('wsse:Security')->value(\$hdr1));
 
 # check what we got back from the service - if it is a
 # simple variable print it elsif it is a Reference to
 # an ARRAY iterate through it and print the values
-    if ( $ans->fault){
-	print $ans->faultcode, " ", $ans->faultstring, "\n";
-    }
-    else{
-	print "\nResult = ".$ans->result." Resource Destroyed\n";	
+    if( defined($ans) ){
+	if ($ans->fault){
+	    print $ans->faultcode, " ", $ans->faultstring, "\n";
+	}
+	else{
+	    print "\nResult = ".$ans->result." Resource Destroyed\n";	
+	}
     }
 }
 print "\n";
