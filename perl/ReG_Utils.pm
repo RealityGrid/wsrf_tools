57a65524f57f5b5b7c1eed46f35ff18dc0783478
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-03-01 12:40:21 +0000

    [perl] New module containing utilities so I don't have to cut and paste code between scripts.  Currently just contains code for making a WSSE header.

---
+++ perl/ReG_Utils.pm
@@ -0,0 +1,55 @@
+#! /usr/bin/env perl
+#BEGIN {
+#    @INC = ( @INC, "/usr/local/WSRF-Lite");
+#};
+
+package ReG_Utils;
+
+use SOAP::Lite;
+use WSRF::Lite;
+use MIME::Base64;
+use Digest::SHA1 qw(sha1 sha1_hex sha1_base64);;
+use strict;
+
+$ReG_Utils::randInitialized = 0;
+
+#----------------------------------------------------
+# Returns a SOAP::Data object containing a completed
+# wsse:UsernameToken
+
+sub makeWSSEHeader {
+
+    my ($username, $password) = @_;
+
+    if($ReG_Utils::randInitialized == 0){
+        # seed the random number generator
+        srand (time ^ $$ ^ unpack "%L*", `ps axww | gzip`);
+        $ReG_Utils::randInitialized = 1;
+    }
+    my $num = rand 1000000;
+    my $nonce = encode_base64("$num", "");
+
+    # Get the time and date
+    my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime(time);
+    # month returned by localtime is zero-indexed and we need to convert to
+    # a four-digit date...
+    my $launch_time = sprintf "%4d-%02d-%02dT%02d:%02d:%02dZ",
+                           $year+1900,$mon+1,$mday,$hour,$min,$sec;
+
+    # Password digest = Base64( SHA-1(nonce + created + password) )
+    my $digest = sha1_base64($nonce . $launch_time . $password);
+    my $user = SOAP::Data->name('wsse:Username' => $username);
+    my $passData = SOAP::Data->name('wsse:Password' => $digest);
+    my $nonceData = SOAP::Data->name('wsse:Nonce' => $nonce);
+    my $created = SOAP::Data->name('wsse:Created' => $launch_time);
+
+    my $hdr1 = SOAP::Data->new(name => 'wsse:UsernameToken',
+                               value => \SOAP::Data->value($user,
+                                                           $passData,
+                                                           $nonceData,
+                                                           $created));
+    return $hdr1;
+}
+
+1;
+
