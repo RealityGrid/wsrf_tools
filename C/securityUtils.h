771c5e1664014605c004dc67d32194fdd0d3e3fa
Andrew Porter <andrew.porter@manchester.ac.uk>
2006-05-30 16:07:14 +0000

    [C] Steering utils lib replaces these

--- C/securityUtils.h
+++
@@ -1,9 +0,0 @@
-#define MAX_LEN 256
-
-struct security_info {
-  char   caCertsPath[MAX_LEN]; 
-  char   myKeyCertFile[MAX_LEN];
-  char   userDN[MAX_LEN];
-};
-
-int getSecurityConfig(struct security_info *sec);
