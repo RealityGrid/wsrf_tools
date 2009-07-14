5a930dbb6d1a488186e4b650a589daec275a30d0
Robert Haines <rhaines@manchester.ac.uk>
2009-07-14 17:41:10 +0100

    [perl] Removed script-generated files from the repository.
    These files are re-generated each time various scripts run so there's no point
    in storing them in the repository.
    
    Also set up a .gitignore so they are ignored when they have been re-written.

--- perl/reg_app_info.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export REG_SGS_ADDRESS=http://methuselah.mvc.mcc.ac.uk:50005/WSRF/SWS/SWS/225101351062163015133
-export REG_PASSPHRASE=red
--- perl/reg_registry_info.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export REG_REGISTRY_EPR=http://methuselah.mvc.mcc.ac.uk:50005/Session/regServiceGroup/regServiceGroup/5435151841064137034689
-export REG_REGISTRY_PASSPHRASE=
